728176e2a5c7fd42e97573cf1f1e278f7c506c8c
Andrey Shchepetov <shchepetov2012@yandex.ru>
2024-03-21 14:46:03 +0300

    На проверку

---
+++ inventory_1.py
@@ -0,0 +1,43 @@
+import json
+
+inventory = {
+    "inventory": [
+        {
+            "item": "Кисти",
+            "category": "Инструменты",
+            "quantity": 5,
+            "minimum_required": 10
+        },
+        {
+            "item": "Краска акриловая",
+            "category": "Материалы",
+            "quantity": 20,
+            "minimum_required": 15
+        }
+    ]
+}
+
+with open('inventory.json', 'w', encoding='utf-8') as write_file:
+    json.dump(inventory, write_file)
+
+with open('inventory.json', 'r', encoding='utf-8') as read_file:
+    json_data = json.load(read_file)
+
+
+def product_quantity(data):
+    shop_list = {}
+    for material in data['inventory']:
+        item = material['item']
+        quantity = material['quantity']
+        min_required = material['minimum_required']
+        required = min_required - quantity
+        if item not in shop_list:
+            if quantity < min_required:
+                shop_list[item] = required
+    return shop_list
+
+
+result = product_quantity(json_data)
+
+for item, required in result.items():
+    print(f"Необходимо закупить: {result} шт.")
